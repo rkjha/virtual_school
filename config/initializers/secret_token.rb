c19d0778615c4b15a3140610886ad130e1eae1ff
Ramesh Jha <rameshjha420@gmail.com>
2012-03-17 19:10:31 +0530

    project started successfully

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VirtualSchool::Application.config.secret_token = 'd07bb14a7287ea5fe373c71ad32154b194800ef7ca5d3b924092df1f03a9c270c172c33b040892609571872e778d532f5980cc9238b92adccba77e882d1147a6'
